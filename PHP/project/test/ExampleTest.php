14ebeb3153d7dd476d2300e4acdc7b326f446b46
Wu Mobius <energy908@gmail.com>
2021-04-08 20:45:46 +0800

    add php directory structure and a basic example test

---
+++ PHP/project/test/ExampleTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Test;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function TestExample()
+    {
+        $this->assertTrue(true);
+    }
+}
