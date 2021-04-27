47a2041ef1ca9e4db31608ad00904689667f521b
AndrewC <memory9573@gmail.com>
2021-04-27 18:43:32 +0800

    add CH2 project

---
+++ C#/CKUT/CH2_Andrew/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CH2_Andrew
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Example
+    {
+        
+    }
+}
