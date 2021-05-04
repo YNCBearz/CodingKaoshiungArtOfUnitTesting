f282fde61b5d5626c55a613b4e3c4c63aa4658cd
Kyo Cheng <kyo.cheng@titansoft.com.sg>
2021-05-04 18:43:52 +0800

    feat(ch3): add sample code

---
+++ C#/CKUT/CKUT.Core/Services/Ch3/No7/ExtensionManagerFactory.cs
@@ -0,0 +1,19 @@
+﻿namespace CKUT.Core.Services.Ch3.No7
+{
+    public class ExtensionManagerFactory
+    {
+        private IFileExtensionManager _fileExtensionManager = null;
+        public IFileExtensionManager Create()
+        {
+            if (_fileExtensionManager != null)
+            {
+                return _fileExtensionManager;
+            }
+            return new FileExtensionManager();
+        }
+        public void SetManager(IFileExtensionManager fileExtensionManager)
+        {
+            _fileExtensionManager = fileExtensionManager;
+        }
+    }
+}
---
+++ C#/CKUT/CKUT.Core/Services/Ch3/No7/LogAnalyzer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CKUT.Core.Services.Ch3.No7
+{
+    public class LogAnalyzer
+    {
+        private IFileExtensionManager _fileExtensionManager;
+
+        public LogAnalyzer()
+        {
+            _fileExtensionManager = new ExtensionManagerFactory().Create();
+        }
+
+        public bool IsValidFileName(string fileName)
+        {
+            try
+            {
+                return _fileExtensionManager.IsValid(fileName);
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+    }
+}
