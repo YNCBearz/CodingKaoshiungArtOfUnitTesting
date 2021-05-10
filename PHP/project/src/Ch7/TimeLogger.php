439263ac73cad64c47c28ec2864c90969fe485e8
YNCBearz <bear19930414@gmail.com>
2021-05-10 21:12:45 +0800

    feat: 7.5 以SystemTime來建立與DateTime的中間層

--- PHP/project/src/Ch7/TimeLogger.php
+++ PHP/project/src/Ch7/TimeLogger.php
@@ -2,13 +2,11 @@
 
 namespace Src\Ch7;
 
-use DateTime;
-
 class TimeLogger
 {
-    public static function createMessage(string $info)
+    public static function createMessage(string $info): string
     {
-        $dateTime = new DateTime();
-        return $dateTime->format("Y-m-d H:i:s")." ".$info;
+        return SystemTime::now()." ".$info;
     }
+
 }
